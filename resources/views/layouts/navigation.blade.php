fda800256438f6c341e773b61b2c07fb4e89fd9c
SsAbrut <michael.gunawan2002@gmail.com>
2021-11-21 21:33:51 +0700

    Fix user modal profile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,32 +1,5 @@
 @if (request()->is('startups*') || (request()->is('ventures*') && auth()->user()->typeable))
 @else
-    {{-- Profile Modal --}}
-    <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Profil Saya</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="d-flex align-items-center">
-                        <img src="{{ Storage::url(auth()->user()->profile_photo_path) == '/storage/' ? asset('icons/default-user-photo.png') : Storage::url(auth()->user()->profile_photo_path) }}"
-                            class="rounded-circle" style="width: 104px; height: 104px;">
-                        <div class="d-flex flex-column ms-4">
-                            <h4 class="fw-bold mb-0">{{ auth()->user()->name }}</h4>
-                            <p class="mb-0">{{ '@' . auth()->user()->username }}</p>
-                            <p>{{ auth()->user()->email }}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <a href="{{ route('users.profile') }}" class="btn bg-base-color text-decoration-none text-white">Edit Profil</a>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <nav class="navbar bg-white navbar navbar-expand-md fixed-top">
         <div class="container">
             <div class="row align-items-center width-100">
@@ -52,6 +25,36 @@
                     </form>
                 </div>
                 @auth
+                    {{-- Profile Modal --}}
+                    <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Profil Saya</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="d-flex align-items-center">
+                                        <img src="{{ Storage::url(auth()->user()->profile_photo_path) == '/storage/' ? asset('icons/default-user-photo.png') : Storage::url(auth()->user()->profile_photo_path) }}"
+                                            class="rounded-circle" style="width: 104px; height: 104px;">
+                                        <div class="d-flex flex-column ms-4">
+                                            <h4 class="fw-bold mb-0">{{ auth()->user()->name }}</h4>
+                                            <p class="mb-0">{{ '@' . auth()->user()->username }}</p>
+                                            <p>{{ auth()->user()->email }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="{{ route('users.profile') }}"
+                                        class="btn bg-base-color text-decoration-none text-white">Edit Profil</a>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="col-md-3 p-0 d-flex justify-content-end">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item dropdown">
