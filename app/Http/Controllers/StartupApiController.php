6073dffbcff4965282302f1d56a2113b0ac88856
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-11-22 10:25:05 +0700

    Testing for updated StartupApiController index

--- app/Http/Controllers/StartupApiController.php
+++ app/Http/Controllers/StartupApiController.php
@@ -12,22 +12,20 @@ class StartupApiController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        $startups = Startup::with('users')
-            ->withCount('users')
-            ->withSum('users.posts', 'views')
+        $startups = Startup::
+            withCount('users')
+            
             ->get();
 
-        // foreach ($startups as $startup) {
-        //     // $startup->loadCount('users');
-        //     $views = 0;
-        //     $upvotes = 0;
-        //     $downvotes = 0;
-        //     foreach ($startup->users as $user) {
-        //         $user->loadSum('posts', 'views');
-        //         $user->loadSum('posts', 'upvote');
-        //         $user->loadSum('posts', 'downvote');
-        //     }
-        // }
+        foreach ($startups as $startup) {
+            foreach($startup->users as $user){
+                foreach($user->posts as $post){
+                    $startup['upvotes'] += $post->upvote;
+                }
+                $user->unsetRelation('posts');
+            }
+            $startup->unsetRelation('users');
+        }
 
         return response()->json([
             'startup' => $startups
